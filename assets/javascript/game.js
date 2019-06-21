38ac307a00220514a4a4884c4f07d886f53d7fe0
Nick Gluch <nickrgluch@yahoo.com>
2019-06-20 23:33:23 -0500

    turnin

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@ console.log(breedToGuess);
 // save the word on a variable
 var breedToGuessPlaceholder = breedToGuess;
 // loop thru the variable lenth and append and _ to the container
-var placeHolder = '_ ';
+var placeHolder = "_ ";
 
 
 
@@ -37,10 +37,53 @@ document.onkeydown = keyHandler;
 document.onkeyup = function () {
     this.onkeydown = keyHandler;
     keyHandler = null;
+
 };
 
+var guessedLetters = [];
+var guessedLetters = document.getElementById("guessesSoFar");
+var correctGuess = [];
 
 
+
+
+
+
+
+
+
+//keylistener fuction
+
+
+class KeyListener {
+
+    keyTyped(KeyEvent, e) {
+
+        if (e === 'breedToGuess'[item]) {
+
+            console.log(KeyListener);
+            breedToGuess.append(placeHolder);
+
+
+        }
+        else {
+            e.push(guessedLetters);
+
+        }
+
+    }
+}
+
+
+//parts I wasn't able to get to work
+//check if letters guessed matched the word to guess
+//if letters guessed matched then replace a placeholder with the correct guessed letter to reveal part 
+// of word
+//if letters dont match then push the guessed letter to already guessed array 
+//if they typed all the correct letters then they win
+//if they typed wrong letters too many times then lose
+//restart game if win or lose
+//create reset code
 
 
 
